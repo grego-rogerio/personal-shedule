b9f3a70da92bc3be1bcd5210de470262ea778149
Rogério Rodrigues Andrade <rogerio.andrade@dbserver.com.br>
2026-03-13 15:19:47 -0300

    inicial

---
+++ src/test/java/com/personal/schedule/personal_shedule/PersonalSheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.schedule.personal_shedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
